e249053fad0ee2259df33efdf295a3736749bd54
dilpr <dilpreetbajwa1@gmail.com>
2024-10-22 02:07:55 -0400

    commit changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,20 +8,20 @@ const Home: React.FC = () => {
       <h1 className="text-4xl font-bold text-gray-800 mb-4">ReactJS Challanges</h1>
       {/* <img src={coding_image} alt="React Logo" className="w-24 mb-6" /> */}
       <p className="text-lg mb-6">Choose an option below:</p>
-      <ul className="space-y-4">
-        <li>
-          <Link to="/reverse-words" className="bg-indigo-600 text-white py-2 px-4 rounded-lg transition duration-300 hover:bg-indigo-500">
-            Reverse Words
-          </Link>
-        </li>
-        <li>
-          <Link to="/sumpairs" className="bg-indigo-600 text-white py-2 px-4 rounded-lg transition duration-300 hover:bg-indigo-500">
-          SumPairs
-          </Link>
-        </li>
-
-
-      </ul>
+      <div className="" >
+        <ul className="flex flex-row justify-content: space-between space-x-7">
+            <li>
+            <Link to="/reverse-words" className="bg-indigo-600 text-white py-2 px-4 rounded-lg transition duration-300 hover:bg-indigo-500">
+                Reverse Words
+            </Link>
+            </li>
+            <li>
+            <Link to="/sumpairs" className="bg-indigo-600 text-white py-2 px-4 rounded-lg transition duration-300 hover:bg-indigo-500">
+            SumPairs
+            </Link>
+            </li>
+        </ul>
+      </div>
     </div>
   );
 };
